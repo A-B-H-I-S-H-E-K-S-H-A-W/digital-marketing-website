ddab6d7428b331bbc877dff7cb6c1ba27376e135
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-09-16 15:03:34 +0530

    Some minor tweaks in design and logic

--- src/components/others/Navbar.jsx
+++ src/components/others/Navbar.jsx
@@ -2,12 +2,13 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import Button from "../ui/Button";
 
 const navLinks = [
   {
     id: 1,
-    item: "Services",
-    link: "/services",
+    item: "Home",
+    link: "/home",
   },
   {
     id: 2,
@@ -16,19 +17,29 @@ const navLinks = [
   },
   {
     id: 3,
-    item: "Contact",
-    link: "/contact",
+    item: "Services",
+    link: "/services",
   },
 ];
 
 export function Navbar() {
   return (
     <div className="relative w-full flex items-center justify-center">
-      <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50")}>
-        <div className="relative rounded-full border border-transparent bg-persian shadow-input flex justify-center space-x-4 px-8 py-6 ">
-          {navLinks.map(({ id, item, link }) => (
-            <MenuItem key={id} link={link} item={item}></MenuItem>
-          ))}
+      <div
+        className={cn(
+          "fixed top-10 inset-x-0 max-w-4xl mx-auto z-50 px-4 md:px-0"
+        )}
+      >
+        <div className="relative rounded-full border border-transparent bg-persian shadow-input flex justify-between items-center px-4 py-4 ">
+          <div>Logo</div>
+          <div className="space-x-4 hidden md:block">
+            {navLinks.map(({ id, item, link }) => (
+              <MenuItem key={id} link={link} item={item}></MenuItem>
+            ))}
+          </div>
+          <div>
+            <Button>Contact Us</Button>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = ({ children, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="cursor-pointer px-8 py-2 rounded-full bg-gradient-to-b from-tekhelet to-french text-foreground focus:ring-2 focus:ring-french hover:shadow-xl transition duration-200"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
